8b836db3784ed06c6cfa9f51a88b4ddd1e68e7d4
johns325 <johns325@seattleu.edu>
2016-05-01 17:52:32 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -6,16 +6,43 @@
    HTML structures and invoke the proper JS code accordingly. Be sure
    to reference the Bootstrap documentation.)
 */
-
+$(document).ready(function(){
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
+var map = L.map('map-container').setView([46.850, -121.760], 15);
 
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
 // the map tiles layers:
+var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+subdomains: ['otile1','otile2','otile3','otile4']
+});
 
+var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+subdomains: ['otile1','otile2','otile3','otile4']
+});
 
+var mapLayers = {
+    "Satellite": satLayer,
+    "Map View": drawLayer
+};
+
+L.control.layers(mapLayers).addTo(map);
+satLayer.addTo(map);
+	
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
+var peakMarker = L.marker([46.852, -121.760]).addTo(map);
+peakMarker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+	
+var pierceContry = L.marker([46.852, -121.758]).addTo(map);
+pierceContry.bindPopup("<b>Welcome to Pierce County!</b><br>The second-most populous county in Washington behind King.");
+	
+var pointSuccess = L.marker([46.846, -121.767]).addTo(map);
+pointSuccess.bindPopup("<b>Welcome to Point Success!</b><br>The address id is Washington 98304.");
+ 
 
+});
